7bbf65c39c4262b3551c857a7e55a49448a9c70f
officialbytenbite-ui <officialbytenbite@gmail.com>
2025-12-29 21:13:42 +0530

    final

--- app.py
+++ app.py
@@ -10,10 +10,16 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 # ---------------- MySQL Config ----------------
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'chefai'
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = ''
+# app.config['MYSQL_DB'] = 'chefai'
+
+app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
+app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
+app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
+app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
+app.config['MYSQL_PORT'] = int(os.getenv("MYSQL_PORT", 3306))
 
 mysql = MySQL(app)
 
@@ -215,5 +221,5 @@ def signup():
     return jsonify({"message": "Signup successful"}), 201
 
 # ---------------- Run App ----------------
-# if __name__ == "__main__":
-#     app.run(port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
