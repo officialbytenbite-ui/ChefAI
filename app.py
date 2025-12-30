d9cb2ecb2dc2237de3ca16da6f75e469f1a1af10
officialbytenbite-ui <officialbytenbite@gmail.com>
2025-12-30 15:53:19 +0530

    final

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 # app.config['MYSQL_PASSWORD'] = ''
 # app.config['MYSQL_DB'] = 'chefai'
 
-app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
-app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
-app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
-app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
+app.config['MYSQL_HOST'] = os.getenv("mysql.railway.internal")
+app.config['MYSQL_USER'] = os.getenv("3306")
+app.config['MYSQL_PASSWORD'] = os.getenv("feZskSjvBBilxvueImAfonHhwRYrzIGC")
+app.config['MYSQL_DB'] = os.getenv("railway")
 app.config['MYSQL_PORT'] = int(os.getenv("MYSQL_PORT", 3306))
 
 mysql = MySQL(app)
